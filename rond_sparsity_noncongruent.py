5fa9952e2723d2ea4d3cb8605c157916e273e06b
JudiKL <45490444+JudiKL@users.noreply.github.com>
2019-01-22 20:56:34 +0100

    Add files via upload

--- rond_sparsity_noncongruent.py
+++ rond_sparsity_noncongruent.py
@@ -1,5 +1,6 @@
 # PCBS : expérimentation visant à voir l'influence de la taille et du nombre de points sur une tâche de comparaison rapide.
-"""Module rond taile congruent qui a pour but d'afficher des ensembles de points dont le rayon est proportionnel au nombre de points"""
+"""Module espacement congruent qui a pour but d'afficher des ensembles de points dont l'espacement est inversement proportionnel au nombre de points"""
+
 
 import pygame 
 import sys
@@ -7,8 +8,6 @@ import sys
 import random
 # Pour avoir des tailles aléatoires
 import math
-import time # sauvegarder sous différents noms
-import os
 
 def most_size_side(compteur_gauche, compteur_droit):
 	"""Fonction qui détermine où seront situés les plus gros cercles, avec un rapport de 1/2"""
@@ -25,6 +24,22 @@ def most_size_side(compteur_gauche, compteur_droit):
 		right_size = 2*left_size
 	return left_size
 	return right_size
+
+def most_sparisty_side(compteur_gauche,compteur_droit):
+	"""Fonction qui détermine où les points seront théoriquement les plus éloignés entre eux"""
+
+	global left_sparsity
+	global right_sparsity
+	left_size = int()
+	right_size = int()
+	if compteur_gauche > compteur_droit :
+		right_sparsity = random.randint(35,40)
+		left_sparsity = 2*right_sparsity
+	else :
+		left_sparsity = random.randint(35,40)
+		right_sparsity = 2* left_sparsity
+	return left_sparsity
+	return right_sparsity
 
 def distance_points(couple1,couple2):
 	"""Fonction controllant la superposition des points pour notre ensemble de points aléatoires"""
@@ -53,7 +68,7 @@ def check_range_dist(couple,liste_couple,dist_max):
 		compteur += 1
 	return overlap
 
-def display_dots_size_cong():
+def display_dots_sparsity_incong():
 	pygame.init()
 
 	screen = pygame.display.set_mode((1600,900)) # Définition de l'écran. La minimale selon les standards actuels est prise par défaut.
@@ -85,9 +100,13 @@ def display_dots_size_cong():
 	nombre_cercles_gauches = int()
 	nombre_cercles_droits = int()
 
-	most_size_side(compteur_gauche, compteur_droit) # Rapport taille/nombres de points
-	radius_gauche = left_size
-	radius_droit = right_size
+
+	radius_gauche = 8
+	radius_droit = 8
+
+	most_sparisty_side(compteur_gauche,compteur_droit)
+	range_dist_max_left = left_sparsity
+	range_dist_max_right = right_sparsity
 
 	coordonees_cercles_gauches = []
 	coordonees_cercles_droits = []
@@ -102,7 +121,7 @@ def display_dots_size_cong():
 		while check_superposition((position_cercle_x_gauche,position_cercle_y_gauche), coordonees_cercles_gauches, radius_gauche): # Boucle qui empêche la superposition de points
 			position_cercle_x_gauche = random.randint(260, 690)
 			position_cercle_y_gauche = random.randint(410, 690)
-			while check_range_dist((position_cercle_x_gauche,position_cercle_y_gauche), coordonees_cercles_gauches, 25): 
+			while check_range_dist((position_cercle_x_gauche,position_cercle_y_gauche), coordonees_cercles_gauches, range_dist_max_left): 
 				position_cercle_x_gauche = random.randint(260, 690)
 				position_cercle_y_gauche = random.randint(410, 690)
 		print((position_cercle_x_gauche,position_cercle_y_gauche)) # Vérification dans la console
@@ -120,7 +139,7 @@ def display_dots_size_cong():
 		while check_superposition((position_cercle_x_droit,position_cercle_y_droit), coordonees_cercles_droits, radius_droit): # Boucle qui empêche la superposition de points
 			position_cercle_x_droit = random.randint(910,1340)
 			position_cercle_y_droit = random.randint(410, 690)
-			while check_range_dist((position_cercle_x_droit,position_cercle_y_droit), coordonees_cercles_droits, 50): 
+			while check_range_dist((position_cercle_x_droit,position_cercle_y_droit), coordonees_cercles_droits, range_dist_max_right): 
 				position_cercle_x_droit = random.randint(910, 1340)
 				position_cercle_y_droit = random.randint(410, 690)
 		pygame.draw.circle(screen, darkGrey, (position_cercle_x_droit,position_cercle_y_droit), radius_droit, 0)
@@ -128,9 +147,6 @@ def display_dots_size_cong():
 		coordonees_cercles_droits.append((position_cercle_x_gauche,position_cercle_y_gauche))
 	print("le nombre de cercles de à droite est de", nombre_cercles_droits)
 
-	if os.path.exists("stimulus_rond_size_up.png"):
-		pygame.image.save(screen, "stimulus_rond_size_up_{}.png".format(int(time.time())))	
-	else:
-		pygame.image.save(screen, "stimulus_rond_size_up{}.png")
 
-display_dots_size_cong()
+	# sauvegarder au format png le stimulus
+	pygame.image.save(screen, "stimulus_rond_sparsity_down.png")
